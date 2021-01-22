817261e2fbac7e970aa78b5afa2372d45412edaf
Sergio Cervantes <50383374+Zappaware@users.noreply.github.com>
2021-01-22 08:20:23 -0600

    Working on the first section of the project

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() { 
+    return (
+    <div className="App">
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <p>-- Add a SearchBar component --</p>
+          <div className="App-playlist">
+            <p>-- Add a SearchResults component --</p>
+            <p>-- Add a Playlist component --</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+ }
+}
+
+
+export default App;
